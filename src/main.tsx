4d9c7537aeb0e071c9773a10176eafa2c75b3f10
Hao Wang <hwang669@asu.edu>
2026-09-06 19:18:05 +0800

    feat: activate XHS mobile landscape behavior

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import './xhs-landscape.css'
 import { seedTengwangDemoIfEmpty } from './lib/tengwangDemo'
+import { installXhsLandscapePreference } from './lib/xhsLandscape'
 import { router } from './router.tsx'
+
+installXhsLandscapePreference()
 
 function renderApp() {
   createRoot(document.getElementById('root')!).render(
